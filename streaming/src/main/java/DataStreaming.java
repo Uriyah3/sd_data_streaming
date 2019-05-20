1143a833ff0777b74b796cabc690a409d534efce
Juan Pablo <juan.rojas.r@usach.cl>
2019-05-20 00:51:20 -0400

    Streaming Implementado

--- streaming/src/main/java/DataStreaming.java
+++ streaming/src/main/java/DataStreaming.java
@@ -1,2 +1,125 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.enums.PNStatusCategory;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+
+import java.util.Arrays;
+
 public class DataStreaming {
+
+    private String pub_key = "pub-c-40a0587c-4daa-4cbd-b655-ccd76a040a39";
+    private String sub_key;
+    private String channelname;
+
+    public DataStreaming(String sub_key,String channelname){
+        this.sub_key = sub_key;
+        this.channelname = channelname;
+    }
+
+    public void executeStreaming(){
+
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey(this.sub_key);
+        pnConfiguration.setPublishKey(this.pub_key);
+
+        PubNub pubnub = new PubNub(pnConfiguration);
+
+        String channelName = this.channelname;
+
+        // create message payload using Gson
+        JsonObject messageJsonObject = new JsonObject();
+        messageJsonObject.addProperty("msg", "hello");
+
+
+
+
+
+        pubnub.addListener(new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+
+
+                if (status.getCategory() == PNStatusCategory.PNUnexpectedDisconnectCategory) {
+                    // This event happens when radio / connectivity is lost
+                }
+
+                else if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+
+                    // Connect event. You can do stuff like publish, and know you'll get it.
+                    // Or just use the connected event to confirm you are subscribed for
+                    // UI / internal notifications, etc
+
+                    if (status.getCategory() == PNStatusCategory.PNConnectedCategory){
+                        pubnub.publish().channel(channelName).message(messageJsonObject).async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                // Check whether request successfully completed or not.
+                                if (!status.isError()) {
+                                    System.out.println("Successfully published to especified channel");
+                                    // Message successfully published to specified channel.
+                                }
+                                // Request processing failed.
+                                else {
+                                    System.out.println("Check 'category' property to find out possible issue");
+
+                                    // Handle message publish error. Check 'category' property to find out possible issue
+                                    // because of which request did fail.
+                                    //
+                                    // Request can be resent using: [status retry];
+                                }
+                            }
+                        });
+                    }
+                }
+                else if (status.getCategory() == PNStatusCategory.PNReconnectedCategory) {
+
+                    // Happens as part of our regular operation. This event happens when
+                    // radio / connectivity is lost, then regained.
+                }
+                else if (status.getCategory() == PNStatusCategory.PNDecryptionErrorCategory) {
+
+                    // Handle messsage decryption error. Probably client configured to
+                    // encrypt messages and on live data feed it received plain text.
+                }
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+                // Handle new message stored in message.message
+                if (message.getChannel() != null) {
+                    // Message has been received on channel group stored in
+                    // message.getChannel()
+                }
+                else {
+                    // Message has been received on channel stored in
+                    // message.getSubscription()
+                }
+
+                JsonElement receivedMessageObject = message.getMessage();
+                JsonObject json = receivedMessageObject.getAsJsonObject();
+
+                //Aqui se sacan los datos necesarios para la bd
+
+                System.out.println(json.get("user").toString());
+                System.out.println(json.get("item").toString());
+
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        });
+
+        pubnub.subscribe().channels(Arrays.asList(channelName)).execute();
+    }
+
+
 }
--- streaming/src/main/java/Main.java
+++ streaming/src/main/java/Main.java
@@ -1,2 +1,12 @@
+
+
+
 public class Main {
+
+
+    public static void main(String[] args){
+
+        DataStreaming streaming = new DataStreaming("sub-c-b0d14910-0601-11e4-b703-02ee2ddab7fe", "pubnub-wikipedia");
+        streaming.executeStreaming();
+    }
 }
